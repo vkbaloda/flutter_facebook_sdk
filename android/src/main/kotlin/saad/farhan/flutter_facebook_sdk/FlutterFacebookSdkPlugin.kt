a8add47ac8d43c4c91e642d088a697c0d06718a1
vipul <kumarvipul685@gmail.com>
2021-10-27 01:14:25 +0530

    ui-thread issue fix2

--- android/src/main/kotlin/saad/farhan/flutter_facebook_sdk/FlutterFacebookSdkPlugin.kt
+++ android/src/main/kotlin/saad/farhan/flutter_facebook_sdk/FlutterFacebookSdkPlugin.kt
@@ -3,6 +3,8 @@ package saad.farhan.flutter_facebook_sdk
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.annotation.NonNull
 import bolts.AppLinks
 import com.facebook.FacebookSdk
